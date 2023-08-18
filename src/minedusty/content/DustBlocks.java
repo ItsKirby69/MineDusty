b7b0084e9f320e77066c39c42109d7cc73624145
ItsKirby69 <117233443+ItsKirby69@users.noreply.github.com>
2023-08-18 17:35:24 +0000

    ores and machines

--- src/minedusty/content/DustBlocks.java
+++ src/minedusty/content/DustBlocks.java
@@ -75,7 +75,24 @@ public class DustBlocks {
             consumeItem(DustItems.quartz, 1);
             consumePower(0.50f);
         }};
+
+        quartzSmelter = new GenericCrafter("quartz-smelter"){{
+            requirements(Category.crafting, with(Items.copper, 30, Items.lead, 25, Items.silicon, 50));
+            craftEffect = Fx.smeltsmoke;
+            outputItem = new ItemStack(DustItems.rosequartz, 1);
+            craftTime = 40f;
+            size = 2;
+            hasPower = true;
+            hasLiquids = false;
+            drawer = new DrawMulti(new DrawDefault(), new DrawFlame(Color.valueOf("ffef99")));
+            ambientSound = Sounds.smelter;
+            ambientSoundVolume = 0.07f;
+
+            consumeItems(with(Items.coal, 1, Items.silicon, 2, Items.thorium, 1));
+            consumePower(0.50f);
+        }};
 	
+
 		//deco blocks
 		aliveTree = new TreeBlock("alive-tree");
 
