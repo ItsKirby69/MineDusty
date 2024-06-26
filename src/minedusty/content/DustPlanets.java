28da07e2ca79360b0a7617c40c90512cd3164277
ItsKirby69 <117233443+ItsKirby69@users.noreply.github.com>
2024-06-26 18:27:50 -0400

    changes on planet

--- src/minedusty/content/DustPlanets.java
+++ src/minedusty/content/DustPlanets.java
@@ -24,64 +24,54 @@ public class DustPlanets {
 
 	public static void load(){
 		gaia = new Planet("gaia", Planets.sun, 1.2f, 3){{
-			generator = new ErekirPlanetGenerator();
+			generator = new SerpuloPlanetGenerator();
 			alwaysUnlocked = true;
-			visible = true;
-			bloom = false;
 			accessible = true;
-			minZoom = 0.9f;
+			minZoom = 0.75f;
 			allowLaunchLoadout = false;
 			allowLaunchSchematics = false;
 			allowLaunchToNumbered = true;
 			allowSectorInvasion = true;
 			clearSectorOnLose = true;
 			defaultCore = DustBlocks.coreNest;
-			orbitRadius = 55;
-			orbitOffset = 12f;
-			atmosphereRadIn = 0.1f;
-			atmosphereRadOut = 0.35f;
+			orbitRadius = 49f;
+			//orbitOffset = 1f;
+			atmosphereRadOut = 0.2f;
 			parent = Planets.sun;
 			updateLighting = true;
-			radius = 1.2f;
 			defaultEnv = 5;
 			ruleSetter = r -> {
 
 			};
 			startSector = 12;
 			iconColor = Color.valueOf("6e8b3d");
-			atmosphereColor = Color.valueOf("44BA23");
+			atmosphereColor = Color.valueOf("0ad00a");
 			hiddenItems.addAll(Items.erekirItems).removeAll(Items.serpuloItems);
 			meshLoader = () -> new MultiMesh(
-				new SunMesh(this,5,1,0.5,2,2.2,3,1f, 
-				Color.valueOf("43718e"),
-				Color.valueOf("437b8e"),
-				Color.valueOf("468f92")),
-				new SunMesh(this,5,1,0.5,2,2.2,3,1f, 
-				Color.valueOf("43718e"),
-				Color.valueOf("437b8e"),
-				Color.valueOf("468f92")),
-				new SunMesh(this,5,1,0.5,2,2.2,3,1f, 
-				Color.valueOf("43718e"),
-				Color.valueOf("437b8e"),
-				Color.valueOf("468f92")),
+				//(P planet, i divisions, d octaves, d persistence, d scl, d pow, d mag, f colorScale, C... colors)
+				new SunMesh(this,5,1,0.5,2,2.2,3,1f,
+				Color.valueOf("43838e"),
+				Color.valueOf("438d8e"),
+				Color.valueOf("44a29f")),
 				//(P planet, i seed, i divisions, f radius, i octaves, f persistence, f scale, f mag, C color1, C color2, i coct, f cper, f cscl, f cthresh)
-				new NoiseMesh(this, 0,6, 0.9f, 3, 1.1f, 1.1f, 3.9f, Color.valueOf("c8cdce"),Color.valueOf("a0babf"), 1, 1.1f, 0.7f, 0.4f)
+				new NoiseMesh(this, 1, 6, 1.15f, 3, 1f, 1.8f, 1.1f, Color.valueOf("b6d7a8"), Color.valueOf("7cba61"), 2, 1.2f, 1f, 0.5f),
+				new NoiseMesh(this, 1, 6, 1.1f, 3, 0.8f, 2.1f, 1f, Color.valueOf("88834C"), Color.valueOf("96814C"), 1, 1.2f, 2.1f, 0.2f),
+				//mountains
+				new NoiseMesh(this, 0, 6, 0.9f, 3, 1.5f, 1.5f, 1.4f, Color.valueOf("c8cdce"),Color.valueOf("a0babf"), 1, 1.9f, 1.7f, 0.4f)
 			);
 			cloudMeshLoader = () -> new MultiMesh(
 				//(P planet, i seed, f speed, f radius, i divisions, C color, i octaves, f persistence, f scl, f thresh)
-				new HexSkyMesh(this, 2, 0.97f, 0.07f, 6, new Color().set(Color.valueOf("c5dbdd")).mul(0.9f).a(0.8f), 2, 0.6f, 1.1f, 0.4f),
-				new HexSkyMesh(this, 0, 0.93f, 0.12f, 6, new Color().set(Color.valueOf("9bb4bf")).mul(0.9f).a(0.8f), 2, 0.5f, 1.0f, 0.3f)
+				new HexSkyMesh(this, 0, 0.87f, 0.13f, 6, new Color().set(Color.valueOf("B8CFC0")).mul(0.9f).a(0.8f), 2, 0.5f, 0.8f, 0.3f),
+				new HexSkyMesh(this, 0, 0.83f, 0.16f, 6, Color.white.cpy().lerp(Color.valueOf("80A499"), 0.55f).a(0.75f), 2, 0.55f, 0.85f, 0.35f)
 			);
-
-			/*
-            cloudMeshLoader = () -> new MultiMesh(
+			/*cloudMeshLoader = () -> new MultiMesh(
                 new HexSkyMesh(this, 11, 0.15f, 0.13f, 5, new Color().set(Pal.spore).mul(0.9f).a(0.75f), 2, 0.45f, 0.9f, 0.38f),
                 new HexSkyMesh(this, 1, 0.6f, 0.16f, 5, Color.white.cpy().lerp(Pal.spore, 0.55f).a(0.75f), 2, 0.45f, 1f, 0.41f)
             );*/
-			//unlockedOnLand = stuff;
+			unlockedOnLand.addAll(DustBlocks.coreNest);
 		}};
 
-		testd = new Planet("tantros", Planets.sun, 1f, 2){{
+		testd = new Planet("blue-thing", DustPlanets.gaia, 0.8f, 1){{
             generator = new TantrosPlanetGenerator();
             meshLoader = () -> new HexMesh(this, 4);
             accessible = true;
@@ -90,11 +80,11 @@ public class DustPlanets {
             atmosphereColor = Color.valueOf("3db899");
             iconColor = Color.valueOf("597be3");
             startSector = 10;
-            atmosphereRadIn = -0.01f;
+            //atmosphereRadIn = -0.01f;
             atmosphereRadOut = 0.3f;
+			minZoom = 0.75f;
             defaultEnv = Env.underwater | Env.terrestrial;
             ruleSetter = r -> {
-
             };
         }};
 
