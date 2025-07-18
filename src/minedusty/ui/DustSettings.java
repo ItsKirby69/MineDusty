105dd35e2f49475d22afc74d0d7f1c36a868fdcb
ItsKirby69 <itsdakirby69@gmail.com>
2025-07-18 15:27:00 -0400

    Settings change

--- src/minedusty/ui/DustSettings.java
+++ src/minedusty/ui/DustSettings.java
@@ -3,14 +3,19 @@ package minedusty.ui;
 import static mindustry.Vars.*;
 
 import arc.Core;
+import arc.scene.ui.ImageButton;
+import arc.scene.ui.layout.Table;
 import mindustry.Vars;
 import mindustry.gen.Icon;
-import mindustry.gen.Tex;
 import mindustry.ui.Styles;
+import mindustry.ui.dialogs.*;
+import mindustry.ui.dialogs.SettingsMenuDialog.SettingsTable.*;
 import minedusty.planets.DustPlanets;
 
+import static mindustry.Vars.ui;
+
 public class DustSettings {
-    
+    // Again, completely from Omaloon
 
     public static void load() {
         ui.settings.addCategory("@setting.dusty-settings-title.title", "minedusty-settings-icon", t -> {
@@ -21,21 +26,36 @@ public class DustSettings {
 
             t.checkPref("@setting.dusty-disable-popup", false);
 
-            t.row();
-            // TODO fix these options disapearing when reseting to default
-            t.button("@setting.dusty-cleartree", 
-            Icon.trash,
-            Styles.flatt, 
-            Vars.iconMed,
-            () -> Vars.ui.showConfirm("@setting.dusty-cleartree-confirm", () -> DustData.resetTree(DustPlanets.theia.techTree)));
             
-            t.button(Icon.github, () ->{
-                String url = "https://github.com/ItsKirby69/MineDusty";
-                if(!Core.app.openURI(url)){
-                    ui.showInfoFade("@linkfail");  
-                    Core.app.setClipboardText(url);
-                }
-            }).size(250f).tooltip("@@setting.dusty-github").right().bottom();
+            // TODO fix these options disapearing when reseting to default
+            t.pref(new TableSetting("github-linko", new Table(c -> {
+                c.button("@setting.dusty-cleartree", Icon.trash, Styles.flatt, Vars.iconMed,() -> 
+                    Vars.ui.showConfirm("@setting.dusty-cleartree-confirm", () -> DustData.resetTree(DustPlanets.theia.techTree)
+                )).width(230f);
+
+                c.button(Icon.github, new ImageButton.ImageButtonStyle(), () ->{
+                    String url = "https://github.com/ItsKirby69/MineDusty";
+                    if(!Core.app.openURI(url)){
+                        ui.showInfoFade("@linkfail");  
+                        Core.app.setClipboardText(url);
+                    }
+                }).size(50f).tooltip("@setting.dusty-github").right().bottom();    
+            })));
+            
         });
+    }
+    
+    public static class TableSetting extends Setting {
+        public Table t;
+
+        public TableSetting(String name, Table table){
+            super(name);
+            this.t = table;
+        }
+
+        @Override
+        public void add(SettingsMenuDialog.SettingsTable table){
+            table.add(t).fillX().row();
+        }
     }
 }
