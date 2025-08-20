9bc11fff4dbf45c475b04d64758c7917db835b45
ItsKirby69 <itsdakirby69@gmail.com>
2025-08-20 14:33:18 -0400

    Drills

--- src/minedusty/blocks/DustDrills.java
+++ src/minedusty/blocks/DustDrills.java
@@ -2,25 +2,39 @@ package minedusty.blocks;
 
 import mindustry.content.*;
 import mindustry.type.Category;
+import mindustry.type.Liquid;
+import mindustry.type.LiquidStack;
 import mindustry.world.*;
+import mindustry.world.blocks.production.Drill;
 import mindustry.world.meta.Env;
 import minedusty.content.DustItems;
+import minedusty.content.DustLiquids;
 import minedusty.world.blocks.production.*;
 
 import static mindustry.type.ItemStack.*;
 
 public class DustDrills {
-    public static Block mechanicalDrill;
+    public static Block mechanicalDrill, chloroDrill;
 
     public static void loadContent(){
-        mechanicalDrill = new TeamDrill("mechanical-drill"){{
+        mechanicalDrill = new ModifiedDrill("mechanical-drill"){{
             requirements(Category.production, with(DustItems.oxidecopper, 15));
             tier = 2;
             drillTime = 700;
             size = 2;
             envEnabled ^= Env.space;
 
-            consumeLiquid(Liquids.water, 0.4f).boost();
+            consumeLiquid(Liquids.water, 0.05f).boost();
+        }};
+
+        chloroDrill = new ModifiedDrill("chloro-drill"){{
+            requirements(Category.production, with(DustItems.oxidecopper, 12, DustItems.chlorophyte, 10));
+            tier = 3;
+            drillTime = 450;
+            size = 2;
+            drawTopUnder = true;
+
+            consumeLiquid(DustLiquids.bioLiquid, 0.1f).boost();
         }};
     }
 }
---
+++ src/minedusty/world/blocks/production/ModifiedDrill.java
@@ -0,0 +1,84 @@
+package minedusty.world.blocks.production;
+
+import arc.Core;
+import arc.graphics.Blending;
+import arc.graphics.g2d.Draw;
+import arc.graphics.g2d.TextureRegion;
+import arc.math.Mathf;
+import arc.util.Nullable;
+import arc.util.Time;
+import mindustry.graphics.Drawf;
+import mindustry.graphics.Layer;
+import mindustry.type.Liquid;
+import mindustry.world.blocks.production.Drill;
+import mindustry.world.consumers.ConsumeLiquidBase;
+import mindustry.world.meta.Stat;
+import mindustry.world.meta.StatUnit;
+import mindustry.world.meta.StatValues;
+
+public class ModifiedDrill extends Drill{
+    public TextureRegion teamRegion;
+    public boolean drawTopUnder = false;
+    
+    public ModifiedDrill(String name){
+        super(name);
+    }
+
+    @Override
+    public void load(){
+        super.load();
+        teamRegion = Core.atlas.find(name + "-team", Core.atlas.find("clear"));
+    }
+
+    public class ModifiedDrillBuild extends DrillBuild {
+        public float boost1, boost2;
+
+        @Override
+        public void draw(){
+            float s = 0.3f;
+            float ts = 0.6f;
+
+            Draw.rect(region, x, y);
+            Draw.z(Layer.blockCracks);
+            drawDefaultCracks();
+
+            Draw.z(Layer.blockAfterCracks);
+            if(drawRim){
+                Draw.color(heatColor);
+                Draw.alpha(warmup * ts * (1f - s + Mathf.absin(Time.time, 3f, s)));
+                Draw.blend(Blending.additive);
+                Draw.rect(rimRegion, x, y);
+                Draw.blend();
+                Draw.color();
+            }
+
+            if(drawTopUnder){
+                Draw.rect(topRegion, x, y);
+                if(drawSpinSprite){
+                    Drawf.spinSprite(rotatorRegion, x, y, timeDrilled * rotateSpeed);
+                }else{
+                    Draw.rect(rotatorRegion, x, y, timeDrilled * rotateSpeed);
+                }
+            } else {
+                if(drawSpinSprite){
+                    Drawf.spinSprite(rotatorRegion, x, y, timeDrilled * rotateSpeed);
+                }else{
+                    Draw.rect(rotatorRegion, x, y, timeDrilled * rotateSpeed);
+                }
+                Draw.rect(topRegion, x, y);
+            }
+
+            if(dominantItem != null && drawMineItem){
+                Draw.color(dominantItem.color);
+                Draw.rect(itemRegion, x, y);
+                Draw.color();
+            }
+
+            if(teamRegion != null){
+                Draw.color(team.color);
+                Draw.rect(teamRegion, x, y);
+                Draw.color();
+            }
+        }
+    }
+}
