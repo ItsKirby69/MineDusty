3bb385a4ff4d932493056865bf48db5ff57c33b6
ItsKirby69 <itsdakirby69@gmail.com>
2025-08-22 12:21:58 -0400

    changed oxide wall speeds

--- src/minedusty/world/blocks/defense/OxideWall.java
+++ src/minedusty/world/blocks/defense/OxideWall.java
@@ -15,7 +15,7 @@ import static arc.Core.*;
 public class OxideWall extends Wall {
 
     public int maxStages = 3;
-    public float stageDur = 60f*3;//3600 * 5f;
+    public float stageDur = 3600 * 5f;
     /** How fast stagetime passes by if rainy weather */
     public float rainAccel = 2f;
 
@@ -55,7 +55,7 @@ public class OxideWall extends Wall {
         }
 
         public void randomStageDur(){
-            currStageDuration = stageDur ;//+ Mathf.random(3600f * 1f, 3600f * 12f);
+            currStageDuration = stageDur + Mathf.random(3600f * 1f, 3600f * 12f);
         }
 
         @Override
