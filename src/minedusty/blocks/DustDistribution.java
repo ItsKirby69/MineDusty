be9f9caaf1b218b036bb492e18da0b3906b73cca
ItsKirby69 <itsdakirby69@gmail.com>
2025-10-22 21:16:41 -0400

    liquid distribution blocks

--- src/minedusty/blocks/DustDistribution.java
+++ src/minedusty/blocks/DustDistribution.java
@@ -5,6 +5,7 @@ import static mindustry.type.ItemStack.with;
 import static minedusty.content.DustItems.aquamerium;
 import static minedusty.content.DustItems.oxidecopper;
 
+import arc.graphics.Color;
 import mindustry.content.Items;
 import mindustry.type.Category;
 import mindustry.world.Block;
@@ -13,6 +14,9 @@ import mindustry.world.blocks.distribution.Junction;
 import mindustry.world.blocks.distribution.OverflowGate;
 import mindustry.world.blocks.distribution.Router;
 import mindustry.world.blocks.distribution.Sorter;
+import mindustry.world.blocks.liquid.Conduit;
+import mindustry.world.blocks.liquid.LiquidJunction;
+import mindustry.world.blocks.liquid.LiquidRouter;
 import minedusty.content.DustItems;
 
 public class DustDistribution {
@@ -20,12 +24,40 @@ public class DustDistribution {
 	public static Block copperConveyor, copperJunction, copperRouter, 
     copperOverflowGate, copperUnderflowGate,
     copperSorter, copperInvertedSorter;
+
+    public static Block aquameriumConduit, aquaLiquidRouter, aquaLiquidJunction;
     
     public static Block aquameriumConveyor;
     public static Block armoredGalenaConveyor; // a heavily armored conveyor. Perhaps instead make it chlorophyte that heals itself?
     public static Block electrumConveyor; // A power requiring stack conveyor?
 
 	public static void loadContent(){
+        // region conduits
+        aquaLiquidRouter = new LiquidRouter("aqua-liquid-router"){{
+            requirements(Category.liquid, with(aquamerium, 2, Items.graphite, 4));
+            liquidCapacity = 150f;
+            underBullets = true;
+            solid = false;
+            explosivenessScale = flammabilityScale = 4f/20f;
+        }};
+
+        aquameriumConduit = new Conduit("aqua-conduit"){{
+            requirements(Category.liquid, with(aquamerium, 1, Items.lead, 1));
+            liquidCapacity = 30f;
+            health = 60;
+            explosivenessScale = flammabilityScale = 8.5f/20f;
+            botColor = Color.white;
+        }};
+
+        aquaLiquidJunction = new LiquidJunction("aqua-liquid-junction"){{
+            requirements(Category.liquid, with(aquamerium, 10, Items.graphite, 6));
+            solid = false;
+
+            ((Conduit) aquameriumConduit).junctionReplacement = this;
+        }};
+        //end region
+
+        // region conveyor
         copperConveyor = new Conveyor("oxide-copper-conveyor"){{
             requirements(Category.distribution, with(DustItems.oxidecopper, 1));
             health = 35;
@@ -80,5 +112,6 @@ public class DustDistribution {
             buildCostMultiplier = 2.5f;
             invert = true;
         }};
+        // end region
 	}
 }
