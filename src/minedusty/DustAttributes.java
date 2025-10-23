c72c23439701cbee730d15c52281faa7c4501215
ItsKirby69 <itsdakirby69@gmail.com>
2025-10-22 21:17:27 -0400

    Adding crystal and turf attribute

--- src/minedusty/DustAttributes.java
+++ src/minedusty/DustAttributes.java
@@ -3,10 +3,13 @@ package minedusty;
 import mindustry.world.meta.Attribute;
 
 public class DustAttributes {
-    public static Attribute chlorophyte, salt;
+    public static Attribute chlorophyte, salt, crystal, turf;
 
     public static void load() {
         chlorophyte = Attribute.add("chlorophyte");
         salt = Attribute.add("salt");
+        crystal = Attribute.add("crystal");
+
+        turf = Attribute.add("turf");
     }
 }
