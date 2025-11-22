37de56814c36683b8ca4eaa996d9ce8af9a560f3
ItsKirby69 <itsdakirby69@gmail.com>
2025-11-21 20:43:14 -0500

    No I guess...??

--- build.gradle.kts
+++ build.gradle.kts
@@ -116,9 +116,9 @@ project(":"){
         add("kapt", entity(":entity"))
 
         compileOnly(mindustry(":core"))
-        compileOnly(mindustry(":core:sources"))
+        // compileOnly(mindustry(":core:sources"))
         compileOnly(arc(":arc-core"))
-        compileOnly(arc(":arc-core:sources"))
+        // compileOnly(arc(":arc-core:sources"))
     }
 
     val jar = tasks.named<Jar>("jar"){
