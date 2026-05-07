f7d540420cff1cf1fd472f3288f0b51b1db8d5b5
ItsKirby69 <itsdakirby69@gmail.com>
2026-05-06 22:20:30 -0400

    Remove redundant rotating text reflection

--- src/minedusty/world/ui/DustSplashFrag.java
+++ src/minedusty/world/ui/DustSplashFrag.java
@@ -1,7 +1,5 @@
 package minedusty.world.ui;
 
-import java.lang.reflect.Field;
-
 import arc.Core;
 import arc.Events;
 import arc.graphics.Color;
@@ -16,28 +14,15 @@ import arc.struct.Seq;
 import arc.util.*;
 import mindustry.core.GameState;
 import mindustry.game.EventType.StateChangeEvent;
-import mindustry.ui.Fonts;
+import mindustry.graphics.Drawf;
 import minedusty.MineDustyMod;
 
 public class DustSplashFrag {
-    // TEMPORARY REFLECTIOn
     public String text;
-    public static Field pageVerticesField, idxField;
-
+    
     public float jiggle = 0f;
     public float splashX = 0f;
     public float splashY = 0f;
-
-    static{
-        try{
-            pageVerticesField = FontCache.class.getDeclaredField("pageVertices");
-            idxField = FontCache.class.getDeclaredField("idx");
-            pageVerticesField.setAccessible(true);
-            idxField.setAccessible(true);
-        }catch(NoSuchFieldException e){
-            throw new RuntimeException("No field! ", e);
-        }
-    }
 
     public void build(Group parent){
         Seq<String> subtitles = MineDustyMod.subtitles;
@@ -60,12 +45,12 @@ public class DustSplashFrag {
             float logoh = logow * (float)logo.height / logo.width;
             splashX = (screenW / 2f) + logow / 2.5f;
             splashY = (screenH - logoh) + logoh / 3.5f;
-            float bouncy = Core.settings.getInt("@setting.dusty-splash-bounciness");
+            float bouncy = Core.settings.getInt("dusty-splash-bounciness");
 
             float rot = 17f + Mathf.sin(Time.time / 15f) * 2f * (bouncy / 10f);
             float scale = Scl.scl() * 5.5f + Mathf.sin(Time.time / 12f) * 0.2f * (bouncy / 10f) + Mathf.sin(jiggle * 2.5f) * (jiggle / 3f);
 
-            text(text, splashX, splashY, rot, Color.yellow, scale, Align.center);
+            Drawf.text(text, splashX, splashY, rot, Color.yellow, scale, Align.center);
         });
 
         parent.addListener(new InputListener(){
@@ -82,55 +67,4 @@ public class DustSplashFrag {
             }
         });
     }
-
-    public static void rotateText(FontCache cache, float angleDeg, float x, float y){
-        try{
-            float[][] pageVertices = (float[][]) pageVerticesField.get(cache);
-            int[] idx = (int[]) idxField.get(cache);
-            float rad = angleDeg * Mathf.degreesToRadians;
-            float cos = (float) Math.cos(rad);
-            float sin = (float) Math.sin(rad);
-
-            for(int i = 0; i < pageVertices.length; i++){
-                float[] verts = pageVertices[i];
-                if(verts == null) continue;
-                for(int ii = 0; ii < idx[i]; ii += 6){
-                    float dx = verts[ii] - x;
-                    float dy = verts[ii + 1] - y;
-                    verts[ii] = x + dx * cos - dy * sin;
-                    verts[ii + 1] = y + dx * sin + dy * cos;
-                }
-            }
-        } catch(IllegalAccessException e){
-            throw new RuntimeException(e);
-        }
-    }
-
-    public static void text(String text, float x, float y, float rotation, Color color, float scale, int align){
-        Font font = Fonts.outline;
-        boolean ints = font.usesIntegerPositions();
-        font.setUseIntegerPositions(false);
-        font.getData().setScale(0.25f / Scl.scl(1f) * scale);
-        font.setColor(color);
-        font.getCache().clear();
-        GlyphLayout layout = font.getCache().addText(text, x, y, 0f, align, false);
-        if(rotation != 0){
-            float verticalFraction = (align & Align.bottom) != 0 ? 1f : (align & Align.top) != 0 ? 0f: 0.5f;
-            rotateText(font.getCache(), rotation, x, y - layout.height * verticalFraction);
-        }
-        if(color.a < 1f){
-            font.getCache().setAlphas(color.a);
-        }
-        font.getCache().draw();
-        font.getData().setScale(1f);
-        font.setColor(Color.white);
-        font.setUseIntegerPositions(ints);
-    }
 }
-
-            // Mat mat = new Mat();
-            // mat.rotate(rot);
-            // Draw.trans(mat);
-            // Drawf.text(text, splashX, splashY, Color.yellow, scale);
-            // Draw.trans();
-            // Draw.reset();
